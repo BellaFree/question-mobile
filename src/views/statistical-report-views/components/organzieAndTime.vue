8b0e51d869e1cfc9d18d9c590c6380fa6b2434cc
祝磊 <lei.zhu@gaialab.cn>
2021-11-15 17:41:26 +0800

    feat: 访店记录

--- src/views/statistical-report-views/components/organzieAndTime.vue
+++ src/views/statistical-report-views/components/organzieAndTime.vue
@@ -1,10 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <!-- 弹层： 执行人  -->
+    <van-popup v-model="organizeShow">内容</van-popup>
+    <!-- 弹层： 时间  -->
+    <van-popup v-model="timeShow">
+      <van-datetime-picker
+          v-model="currentDate"
+          @confirm="popupDateConfirm"
+          @cancel="popupDateCancel"
+          type="date"
+          title="选择年月日"
+          :min-date="minDate"
+          :max-date="maxDate"
+          ref="detetimePicker"
+      />
+    </van-popup>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: "组织"
+  name: "组织",
+  data() {
+    return {
+      // 时间显隐控制
+      timeShow: false,
+      // 组织显隐控制
+      organizeShow: false,
+      // 当前选择时间
+      currentDate: '',
+    }
+  },
+  methods: {
+    // 时间 确认
+    popupDateConfirm() {
+
+    },
+    // 时间  取消
+    popupDateCancel() {
+
+    }
+  }
 }
 </script>
 
