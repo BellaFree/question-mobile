798e5434b25218e08ad76fc0ed5508b2aa84e495
evage <>
2021-12-11 14:02:48 +0800

    add upload delete

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="demo">
-      <van-uploader :after-read="afterRead" v-model="fileList" multiple />
+      <!-- <van-uploader :after-read="afterRead" v-model="fileList" multiple /> -->
+
+      <van-uploader 
+          v-model="fileList" 
+          :after-read="afterRead" 
+          :max-size="5012 * 1024" 
+          :before-delete='beforeDeleteBack' 
+          upload-text=""
+          multiple
+          :max-count="3"
+          deletable = true
+          @oversize="onOversize"
+          capture="camera" accept="image" 
+	    />
+
   </div>
 </template>
 
@@ -25,10 +39,10 @@ export default {
   data() {
     return {
       fileList: [
-        { url: 'https://img.yzcdn.cn/vant/leaf.jpg' },
+        // { url: 'https://img.yzcdn.cn/vant/leaf.jpg' },
         // Uploader 根据文件后缀来判断是否为图片文件
         // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
-        { url: 'https://cloud-image', isImage: true }
+        // { url: 'https://cloud-image', isImage: true }
       ]
     }
   },
@@ -42,6 +56,12 @@ export default {
     afterRead(file) {
       // 此时可以自行将文件上传至服务器
       console.log(file);
+    },
+    beforeDeleteBack() {
+      console.log('3');
+    },
+    onOversize() {
+      console.log('onOversize');
     }
   },
 }
