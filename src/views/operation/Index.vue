1361f8a3fbee2eca43d48ec6a52cecc007321ac0
qichao.yang <qichao.yang@gaialab.cn>
2021-12-28 16:18:59 +0800

    去除楼层切换插件

--- src/views/operation/Index.vue
+++ src/views/operation/Index.vue
@@ -311,6 +311,7 @@ export default {
     this.map = new AMap.Map('container', {
       resizeEnable: true,
       zoom: 5,
+      showIndoorMap: false,
       // center: [120.581807, 31.292088],//苏州
     });
     this.geolocation = new AMap.Geolocation({
