29d7cb261a70a6a29a10aa73a4ff85cd1aa0db17
qichao.yang <qichao.yang@gaialab.cn>
2021-12-28 17:31:19 +0800

    下载基盘pdf优化

--- src/components/StoreDetail.vue
+++ src/components/StoreDetail.vue
@@ -653,11 +653,11 @@ export default {
         }else if(this.showData.bpCode){
           let res = await MAP_API.getPdfReport(`?bpCode=${this.showData.bpCode}`);
           if (res && res.data && res.code == 200) {
-            Toast.success(res.data.message);
-            if(res.data.pdfUrl){
-              window.open(res.data.pdfUrl)
+            if(res.data.pdfPath) {
+              window.open(res.data.pdfPath)
+            } else {
+              Toast.success(res.data.message);
             }
-            
           }else{
             Toast.fail(res.message)
           }
