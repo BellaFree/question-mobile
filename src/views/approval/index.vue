b17508959355dd090a4ecf8ab0a722d8b24cdbb3
祝磊 <lei.zhu@gaialab.cn>
2023-01-05 14:54:41 +0800

    feat: 基盘审批页面 接口对接

--- src/views/approval/index.vue
+++ src/views/approval/index.vue
@@ -11,35 +11,46 @@
     </div>
     <div class="approval-content">
       <div class="approval-filter">
-        <div class="approval-filter-left">
-          <div class="approval-filter-item" @click="showStatusPicker">
-            审批状态
-            <svg-icon icon-class="sanjiao" class-name="filter-icon" />
+        <div class='approval-filter-top'>
+          <div class="approval-filter-left">
+            <div class="approval-filter-item" @click="showStatusPicker">
+              {{ approveStatus ? approveStatus['name']: '审批状态' }}
+              <svg-icon icon-class="sanjiao" class-name="filter-icon" />
+            </div>
+            <div class="approval-filter-item" @click="showCityPicker">
+              {{ cityName ? cityName : '城市' }}
+              <svg-icon icon-class="sanjiao" class-name="filter-icon" />
+            </div>
           </div>
-          <div class="approval-filter-item" @click="showCityPicker">
-            城市
-            <svg-icon icon-class="sanjiao" class-name="filter-icon" />
+          <div class="approval-filter-right" @click="showSearchKey = true">
+            <van-icon name="search" size="20" />
           </div>
         </div>
-        <div class="approval-filter-right">
+        <div v-if="showSearchKey" class="approval-filter-bottom">
           <van-search
             v-model="searchKey"
             show-action
             class="approval-filter-key"
             left-icon=""
-            placeholder="请输入搜索关键词">
+            placeholder="请输入搜索关键词"
+            clearable>
             <template #action>
               <div class="search-btn">搜索</div>
             </template>
           </van-search>
+          <span class="clearable" @click="showSearchKey = false">取消</span>
         </div>
       </div>
       <div class="approval-box">
-        <div class="approval-box-item">
-          <h5>小明提交的基盘开发审批申请 <span>(驳回)</span></h5>
-          <p>基盘名称：</p>
-          <p>申请时间：</p>
-          <p>城市：</p>
+        <div
+          v-for="item of list"
+          :key="item.id"
+          class="approval-box-item"
+          @click="redirectDetail(item)">
+          <h5>{{ item.approveName }} <span :style="approveStatusFun(item.approveStatus)">({{ item.approveStatus | approveStatusTxt }})</span></h5>
+          <p>基盘名称：{{ item.bpName }}</p>
+          <p>申请时间：{{ item.createTime }}</p>
+          <p>城市：{{ item.provinceName }}{{ item.cityName }}{{ item.districtName }}</p>
         </div>
       </div>
     </div>
@@ -48,20 +59,30 @@
       <van-picker
         title="审批状态"
         show-toolbar
-        :columns="statusPickerColumn" />
+        :columns="statusPickerColumn"
+        @confirm="onConfirm"
+        @cancel="showStatusPicker">
+        <template #option="option">
+          {{ option.name }}
+        </template>
+      </van-picker>
     </van-popup>
     <!--城市 -->
     <van-popup v-model="dialogCityPickerVisible" round position="bottom">
-      <van-picker
+      <van-cascader
+        v-model="city"
         title="城市"
-        show-toolbar
-        :columns="cityPickerColumn" />
+        :options="cityPickerColumn"
+        :field-names="{ text: 'name', value: 'regionCode', children: 'children' }"
+        class="dicos-van-tab"
+        @close="dialogCityPickerVisible = false"
+        @finish="cityChange" />
     </van-popup>
   </div>
 </template>s
 
 <script>
-
+import approveApi from '@api/approve_api';
 export default {
   name: 'Index',
   subtitle() {
@@ -75,6 +96,7 @@ export default {
   },
   data() {
     return {
+      userInfo: JSON.parse(window.sessionStorage.getItem('userInfo')) || '',
       // tab
       tabOptions: [
         {
@@ -92,13 +114,42 @@ export default {
       ],
       currentViewTabCode: 1,
       dialogStatusPickerVisible: false,
-      statusPickerColumn: ['杭州', '宁波', '温州', '绍兴', '湖州', '嘉兴', '金华', '衢州'],
+      statusPickerColumn: [],
+      approveStatus: '-1',
       dialogCityPickerVisible: false,
-      cityPickerColumn: ['杭州', '宁波', '温州', '绍兴', '湖州', '嘉兴', '金华', '衢州'],
-      searchKey: ''
+      cityPickerColumn: [],
+      city: '',
+      cityName: '',
+      searchKey: '',
+      showSearchKey: false,
+      list: []
     };
   },
+  filters: {
+    approveStatusTxt(val) {
+      const map = { 0: '已撤销', 1: '待审批', 2: '审批通过', 3: '驳回' };
+      return map[val];
+    }
+  },
+  mounted() {
+    this.getBaseData();
+    this.getList();
+  },
   methods: {
+    approveStatusFun(val) {
+      let styleMap = {
+        0: '#19BE6B',
+        1: '#F5A623',
+        2: '#19BE6B',
+        3: '#D0021B'
+
+      };
+      return {
+        ...styleMap,
+        fontSize: '15px',
+        fontWeight: '400',
+      };
+    },
     chooseThisType(code) {
       this.currentViewTabCode = code;
     },
@@ -107,6 +158,68 @@ export default {
     },
     showCityPicker() {
       this.dialogCityPickerVisible = !this.dialogCityPickerVisible;
+    },
+    /**
+     * 获取基础数据
+     */
+    getBaseData() {
+      approveApi.getApproveStatus()
+        .then(res => {
+          if (res.code === 200) {
+            this.statusPickerColumn = res.data;
+          }
+        });
+
+      approveApi.getApproveCity()
+        .then(res => {
+          if (res.code === 200) {
+            this.cityPickerColumn = this.handlerCityData(res.data);
+          }
+        });
+    },
+    // 城市数据处理
+    handlerCityData(data) {
+      for (let item of data) {
+        if (item.children && item.children.length > 0) {
+          this.handlerCityData(item.children);
+        } else {
+          delete item.children;
+        }
+      }
+      return data;
+    },
+    // 获取审批数据
+    getList() {
+      approveApi.getApproveList({
+        approveStatus: this.approveStatus,
+        cityCodes: [],
+        name: this.searchKey,
+        type: this.currentViewTabCode,
+        userId: this.userInfo && this.userInfo.tuId
+      })
+        .then(res => {
+          console.info(res);
+          if (res.code === 200) {
+            this.list = res.data;
+          }
+        });
+    },
+    // 选取城市
+    cityChange(val) {
+      console.info(val);
+      this.cityName = val['selectedOptions'][val['tabIndex']]['name'];
+      this.showCityPicker();
+      this.getList();
+    },
+    onConfirm(value) {
+      this.showStatusPicker();
+      this.getList();
+      this.approveStatus = value;
+    },
+    // 跳转至审批基盘详情
+    redirectDetail(item) {
+      console.info(item);
+      // todo 待
     }
   }
 };
@@ -164,11 +277,14 @@ export default {
   }
   .approval-filter{
     display: flex;
-    height: 40px;
     border-bottom: 1px solid #E6E6E6;
-    align-items: center;
-    justify-content: space-between;
     position: relative;
+    flex-direction: column;
+    .approval-filter-top{
+      display: flex;
+      height: 40px;
+      justify-content: space-between;
+    }
     &-item{
       display: flex;
       align-items: center;
@@ -183,15 +299,29 @@ export default {
     }
     &-right{
       display: flex;
-      background: #F3F3F3;
-      border-radius: 18px;
+      align-items: center;
+      padding-right: 10px;
+    }
+    .filter-icon{
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+    }
+    .approval-filter-bottom{
+      display: flex;
+      align-items: center;
+      padding-bottom: 9px;
+      .approval-filter-key{
+        background: #F3F3F3 !important;
+        border-radius: 18px;
+      }
       ::v-deep {
         .van-search{
           padding: 0;
           background-color: transparent;
         }
         .van-search__content{
-          width: 100px;
+          width: 230px;
           height: 24px;
           background: #F3F3F3;
           border-radius: 18px;
@@ -211,18 +341,20 @@ export default {
       .search-btn{
         width: 34px;
         height: 20px;
-        line-height: 20px;
+        line-height: 21px;
         background: #19BE6B;
         border-radius: 10px;
         font-size: 11px;
         font-weight: 400;
         color: #FFFFFF;
+        margin-right: 3px;
       }
-    }
-    .filter-icon{
-      display: inline-block;
-      width: 20px;
-      height: 20px;
+      .clearable{
+        font-size: 12px;
+        font-weight: 400;
+        color: #666666;
+        margin-left: 15px;
+      }
     }
   }
   .approval-box{
@@ -247,6 +379,15 @@ export default {
         font-weight: 400;
         color: #333333;
         line-height: 20px;
+      }
+    }
+  }
+  .dicos-van-tab{
+    line-height: normal;
+    ::v-deep{
+      .van-tab{
+        line-height: normal;
+        flex: 0 0 33%;
       }
     }
   }
